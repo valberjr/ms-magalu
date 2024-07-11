8d8196bb711f3b50e461192fd2eafe1d51a22eea
valberjr <valber@duck.com>
2024-07-11 15:43:39 -0300

    first commit

---
+++ src/main/java/com/example/ms_magalu/MsMagaluApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_magalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMagaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMagaluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ms_magalu/MsMagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_magalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMagaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
